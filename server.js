de1117f35f2187abb8ee3dcea14098ca181e78dc
mmumm1 <sporran-toxin3k@icloud.com>
2026-03-24 19:54:38 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ app.post("/image", (req, res) => {
   res.send("OK");
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
